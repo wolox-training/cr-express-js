c8817ea687343e69644771d980f806932bfa09ea
cramallo <cramallo65@gmail.com>
2019-06-26 12:00:14 -0300

    creo servicios para album:  getAll y getPhotosAlbum

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,14 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const albumController = require('./controllers/album');
+
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.get('/albums', albumController.getAll);
+  app.get('/albums/:id/photos', albumController.getPhotosAlbum);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
+
+
