8aab5f26611665bc1e0c3eabcb22682cdeb2d9c3
cramallo <cramallo65@gmail.com>
2019-06-26 15:35:44 -0300

    eslint corrections

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,28 +1,33 @@
 const albumService = require('../services/album');
 
-//GET ALL THE ALBUMS
-const getAll = (req, res, next) => {    
-    albumService.getAll().then(albums => {        
-        res.status(200).send(albums);        
-    }).catch(error => {
-        throw(error)
+// GET ALL THE ALBUMS
+const getAll = (req, res) => {
+  albumService
+    .getAll()
+    .then(albums => {
+      res.status(200).send(albums);
+    })
+    .catch(error => {
+      throw error;
     });
 };
 
-//GET THE PHOTOS OF AN ALBUM
-const getPhotosAlbum = (req, res, next) => {
-    let id = req.params.id;
+// GET THE PHOTOS OF AN ALBUM
+const getPhotosAlbum = (req, res) => {
+  const { id } = req.params;
 
-    albumService.getPhotosAlbum(id).then(albums=>{
-        if(albums){
-            res.status(200).send(albums);
-        }else{
-            res.status(404).send('NOT FOUND');
-        }
-    }).catch(error=>{
-        throw(error);
+  albumService
+    .getPhotosAlbum(id)
+    .then(albums => {
+      if (albums) {
+        res.status(200).send(albums);
+      } else {
+        res.status(404).send('NOT FOUND');
+      }
+    })
+    .catch(error => {
+      throw error;
     });
-}
-
+};
 
 module.exports = { getAll, getPhotosAlbum };
