c0857c5f0aa25ffc06d691c3d56ac407e71b6e87
cramallo <cramallo65@gmail.com>
2019-06-26 16:55:40 -0300

    removing trash

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,3 @@
-// const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
 const albumController = require('./controllers/album');
 
@@ -6,7 +5,4 @@ exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', albumController.getAll);
   app.get('/albums/:id/photos', albumController.getPhotosAlbum);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
