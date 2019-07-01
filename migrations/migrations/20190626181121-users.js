062dce2a830813ddfa21beffede6a9eca9496a33
cramallo <cramallo65@gmail.com>
2019-07-01 10:52:17 -0300

    camelcase in user model and snake_case in user migration

--- migrations/migrations/20190626181121-users.js
+++ migrations/migrations/20190626181121-users.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
-      lastname: {
+      last_name: {
         type: Sequelize.STRING,
         allowNull: false
       },
