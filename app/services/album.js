5fbea8a54063654ae933ca0adcadbdfb7ba7ca70
cramallo <cramallo65@gmail.com>
2019-07-01 10:38:32 -0300

    pr corrections

--- app/services/album.js
+++ app/services/album.js
@@ -3,7 +3,7 @@ const { albums_api } = require('../../config').api_services;
 const { defaultError } = require('../errors');
 
 const getAllAlbums = () =>
-  rp(`${albums_api}album`)
+  rp(`${albums_api}albums`)
     .then(res => JSON.parse(res))
     .catch(err => Promise.reject(defaultError(err.message)));
 
