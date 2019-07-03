157775975eb01e5da4eeee5f4db362915d536f4d
Charly <cramallo65@gmail.com>
2019-07-03 14:26:45 -0300

    User Model (#4)
    * Kickoff
    
    * creo servicios para album:  getAll y getPhotosAlbum
    
    * eslint corrections
    
    * creo servicios para album:  getAll y getPhotosAlbum
    
    * eslint corrections
    
    * removing trash
    
    * corrections  applied
    
    * corrections in user model
    
    * pr corrections
    
    * pr corrections
    
    * pr corrections
    
    * merge apiAlbums
    
    * camelcase in user model and snake_case in user migration
    
    * camelcase in user model and snake_case in user migration
    
    * timestamps in user model and migration
    
    * using options in user service
    
    * using options in user service
    
    * createdAt and updatedAt corrections
    
    * changes in travis.yml to avoid error in github
    
    * changes in travis.yml to avoid error in github
    
    * underscored in user model

---
+++ app/models/user.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    'user',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'last_name'
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    { underscored: true }
+  );
---
+++ migrations/migrations/20190626181121-users.js
@@ -0,0 +1,35 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        unique: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        type: Sequelize.DATE
+      }
+    }),
+  down: queryInterface => queryInterface.dropTable('users')
+};
