128c0057534f5d82b421d5bf08dcde911940bec2
cramallo <cramallo65@gmail.com>
2019-07-15 17:58:18 -0300

    changes in db and env

--- config/db.js
+++ config/db.js
@@ -18,10 +18,10 @@ module.exports = {
     logging: false
   },
   production: {
-    username: config.username,
-    password: config.password,
-    database: config.name,
-    host: config.host,
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST_PROD,
     dialect: 'postgres',
     logging: false
   }
