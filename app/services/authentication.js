b3cfffc37c74305020e337ec93f27734cea7c87a
cramallo <cramallo65@gmail.com>
2019-07-15 11:24:45 -0300

    second pr corrections

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -7,6 +7,5 @@ exports.generateToken = user => {
     name: user.name,
     lastName: user.lastName
   };
-  const token = jwt.encode(userAux, secret);
-  return token;
+  return jwt.encode(userAux, secret);
 };
