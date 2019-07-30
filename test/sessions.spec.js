c48c80464e5486dd02032f137fbf47b5fdc26c4d
cramallo <cramallo65@gmail.com>
2019-07-30 09:39:56 -0300

    invalidate tokens tests

--- test/sessions.spec.js
+++ test/sessions.spec.js
@@ -19,28 +19,48 @@ const userData = {
   password: '$2a$10$4sUmMDqL/Ux1rqGIyxka5OljqC.pZHyIPxvVsMsV6wc7Ro1xBHwQC'
 };
 
-const userDataToEndpoint = {
-  email: 'jose@wolox.com.ar',
-  name: 'jose',
-  lastName: 'perez',
-  password: 'asdasdasd4566'
-};
-// TERMINAR
 describe('POST /users/sessions/invalidate_all - invalidate all the user sessions', () => {
   const compareDates = (newDate, oldDate) => new Date(newDate) - new Date(oldDate) >= 0;
+
   it('should succes with updated user base token date', done => {
     createUserModel(userData).then(createdUser => {
       const tokenRegularUser = authenticationService.generateToken(createdUser);
       request(app)
         .post('/users/sessions/invalidate_all')
         .set('Authorization', `Bearer ${tokenRegularUser}`)
-        .send(userDataToEndpoint)
+        .send()
         .then(updatedUser => {
           expect(updatedUser.status).toBe(201);
           expect(compareDates(updatedUser.body.baseAllowedDateToken, createdUser.baseAllowedDateToken)).toBe(
             true
           );
           done();
+        });
+    });
+  });
+
+  it('should fail for invalid token', done => {
+    createUserModel(userData).then(createdUser => {
+      const tokenRegularUser = authenticationService.generateToken(createdUser);
+      request(app)
+        .post('/users/sessions/invalidate_all')
+        .set('Authorization', `Bearer ${tokenRegularUser}`)
+        .send()
+        .then(updatedUser => {
+          expect(updatedUser.status).toBe(201);
+          expect(compareDates(updatedUser.body.baseAllowedDateToken, createdUser.baseAllowedDateToken)).toBe(
+            true
+          );
+          request(app)
+            .post('/users/sessions/invalidate_all')
+            .set('Authorization', `Bearer ${tokenRegularUser}`)
+            .send()
+            .then(res => {
+              expect(res.status).toBe(400);
+              expect(res.body.message).toBe('invalid token');
+              expect(res.body.internal_code).toBe('bad_request_error');
+              done();
+            });
         });
     });
   });
