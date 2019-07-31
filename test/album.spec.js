541752b4b32f1f820178f9f9410e5c5bfbb5c4bd
cramallo <cramallo65@gmail.com>
2019-07-31 13:04:40 -0300

    corrections in album tests

--- test/album.spec.js
+++ test/album.spec.js
@@ -90,7 +90,7 @@ describe('/POST /albums/:id - user purchases an album', () => {
 });
 
 describe('GET /users/:userId/albums - list of bought albums', () => {
-  it('should success with the albums bought by an user', done => {
+  it('should success with the albums bought by the logged regular user', done => {
     createUserModel(regularUser).then(createdUser => {
       const token = authenticationService.generateToken(createdUser);
       buyAlbum(createdUser.id, 2).then(purchasedAlbum => {
@@ -106,7 +106,7 @@ describe('GET /users/:userId/albums - list of bought albums', () => {
     });
   });
 
-  it('should success with the albums bought by an user', done => {
+  it('should success with the albums bought by an user consulted by an adminUser', done => {
     createUserModel(adminUser).then(adminUserCreated => {
       const token = authenticationService.generateToken(adminUserCreated);
       createUserModel(regularUser).then(regularUserCreated => {
@@ -124,7 +124,7 @@ describe('GET /users/:userId/albums - list of bought albums', () => {
     });
   });
 
-  it('should fail for not allowed role to access another user data', done => {
+  it('should fail for not allowed role to access to another user data', done => {
     const anotherRegularUser = {
       email: 'carlos@wolox.com.ar',
       name: 'carlos',
